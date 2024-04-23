8ee581281b6fbc271fcfef305dd8777d44ba9515
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-23 16:09:27 +0200

    Update metadata

--- app/layout.js
+++ app/layout.js
@@ -4,7 +4,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
-  description: "Wizardshot",
+  title: "Wizardshot",
 };
 
 export default function RootLayout({ children }) {
