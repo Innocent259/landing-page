7c9d69aaf53d4c38550b7bbbcc9ecbc09632a6c4
Innocent NDAHIMANA <ndahimanainnocent842@gmail.com>
2024-04-25 22:23:53 +0200

    Display Main and Features components

--- app/page.js
+++ app/page.js
@@ -1,9 +1,13 @@
 import Navbar from "@/components/Navbar";
+import Main from "@/components/Main"
+import Features from "@/components/Features";
 
 export default function Home() {
   return (
-    <main className="flex flex-col justify-between p-6 lg:px-28">
+    <main className="flex flex-col gap-6 p-6 md:px-28">
       <Navbar />
+      <Main />
+      <Features />
     </main>
   );
 }
